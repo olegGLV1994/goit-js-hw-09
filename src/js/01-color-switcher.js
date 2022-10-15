e98702e2d702e655b6b41806319e4985e931c228
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-15 11:38:47 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,9 @@
-
+const btnStart = document.querySelector('button');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+const onCloseClick = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
