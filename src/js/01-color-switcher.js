2b9b44318ad58c55eadc290eba19f18879f3f28e
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-15 14:04:51 +0300

    9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,7 +3,21 @@ const btnStop = document.querySelector('button[data-stop]');
 const bodyEl = document.querySelector('body');
 btnStart.addEventListener('click', onClickStart);
 btnStop.addEventListener('click', onClickStop);
-const onCloseClick = null;
+let onCloseClick = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickStart() {
+  onCloseClick = setInterval(() => {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickStop() {
+  clearInterval(onCloseClick);
+  btnStop.disabled = true;
+  btnStart.disabled = false;
 }
