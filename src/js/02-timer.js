0c15ea31d073609de6edb9b057c54257ee58e101
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-17 22:14:12 +0300

    9.2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,49 +1,58 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+const refs = {
+  btnStart: document.querySelector('button'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+refs.btnStart.disabled = true;
+let onClickStartTimer = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] < new Date()) {
+      window.alert('Please choose a date in the future');
+    } else if (selectedDates[0] > new Date()) {
+      refs.btnStart.disabled = false;
+    }
+    refs.btnStart.addEventListener('click', () => {
+      refs.btnStart.disabled = true;
+      onClickStartTimer = setInterval(() => {
+        const allDay = selectedDates[0] - new Date();
+        const time = convertMs(allDay);
+        onStartValue(time);
+        if (selectedDates[0] === new Date()) {
+          clearInterval(onClickStartTimer);
+        }
+      }, 1000);
+    });
   },
 };
+
 flatpickr('input#datetime-picker', options);
 
-const refs = {
-  btnStart: document.querySelector('button'),
-  value: document.querySelectorAll('.value'),
-};
-refs.btnStart.addEventListener('click', onStartButton);
-
-const sale = {
-  start() {
-    const days = Date.now();
-    setInterval(() => {
-      const dayInterval = Date.now();
-      const allDay = dayInterval - days;
-      const time = convertMs(allDay);
-      console.log(time);
-    }, 1000);
-  },
-};
-sale.start();
+function onStartValue({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
