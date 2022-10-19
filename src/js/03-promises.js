26fa557a459eaf54af82b86fac4e7d2d3d87e740
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-19 10:22:04 +0300

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,28 @@
+import notiflix from 'notiflix';
+const refs = {
+  form: document.querySelector('form'),
+  btn: document.querySelector('button'),
+};
+
+refs.form.addEventListener('sumbit', createPromise);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
